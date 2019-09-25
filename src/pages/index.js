72d89a54df0b54d6e9e4e8f24402e41c4b942c4f
Scott Malmberg <scott.malmberg@icloud.com>
2019-09-25 09:44:17 -0700

    Adding public folder to Git

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default () =>
     <div id="scott-container" />
     <div id="hero-container">
         <h1>Scott<br/>Malmberg</h1>
-        <i className="fab fa-github"></i>
-        <i className="fab fa-linkedin"></i>
+        <a href="https://github.com/ScottMalmberg" target="_blank"><i className="fa fa-github"></i></a>
+        <a href="https://linkedin.com/in/ScottMalmberg" target="_blank"><i className="fa fa-linkedin"></i></a>
     </div>
 </div>
