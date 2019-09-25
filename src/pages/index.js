1ad8692c2c8a4210dbba9519d1b97dc379f57ba1
Scott Malmberg <scott.malmberg@icloud.com>
2019-09-25 07:54:20 -0700

    Structuring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>I'm Scott!</div>
+export default () => 
+
+<div className="container" id="app-container">
+    <div id="scott-container" />
+    <div id="hero-container">
+        <h1>Scott<br/>Malmberg</h1>
+        <i className="fab fa-github"></i>
+        <i className="fab fa-linkedin"></i>
+    </div>
+</div>
