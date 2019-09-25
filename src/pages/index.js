bf51ee05b8038ab0f839824c4098d6276bfc5722
Scott Malmberg <scott.malmberg@icloud.com>
2019-09-25 15:16:27 -0700

    Adding bootstrap, FA, and SASS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
+import "../styles/app.scss"
 
 export default () => 
 
 <div className="container" id="app-container">
-    <div id="scott-container" />
-    <div id="hero-container">
+    <div id="scott-container" className="col"/>
+    <div id="hero-container" className="col">
         <h1>Scott<br/>Malmberg</h1>
-        <a href="https://github.com/ScottMalmberg" target="_blank"><i className="fa fa-github"></i></a>
-        <a href="https://linkedin.com/in/ScottMalmberg" target="_blank"><i className="fa fa-linkedin"></i></a>
+        <a href="https://github.com/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+        <a href="https://linkedin.com/in/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
     </div>
 </div>
