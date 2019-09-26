f77a6df0cfc2d6826ae6bbd5be7150731ca6cf6b
Scott Malmberg <scott.malmberg@icloud.com>
2019-09-25 19:57:33 -0700

    Front page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import "../styles/app.scss"
 
 export default () => 
 
-<div className="container" id="app-container">
-    <div id="scott-container" className="col"/>
+<div id="app-container">
+    <div id="scott-container" className="col"></div>
     <div id="hero-container" className="col">
         <h1>Scott<br/>Malmberg</h1>
-        <a href="https://github.com/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
-        <a href="https://linkedin.com/in/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+        <div id="icon-container">
+            <a href="https://github.com/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+            <a href="https://linkedin.com/in/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+        </div>
     </div>
 </div>
