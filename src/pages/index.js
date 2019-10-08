ac3baa1148bc2353ce475c8176f321a3668886f5
Scott Malmberg <scott.malmberg@icloud.com>
2019-10-08 15:53:16 -0700

    Adding FontAwesome React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 import Descriptor from "../components/Descriptor.js"
 import Layout from "../styles/Layout"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 require("../styles/app.scss")
+
+library.add(fab);
 
 export default () => 
 
@@ -11,8 +16,8 @@ export default () =>
         <h1 style={{color: '#F0F3BD'}}><strong>Scott<br/>Malmberg</strong></h1>
         <Descriptor />
         <div className="icon-container">
-            <a href="https://github.com/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
-            <a href="https://linkedin.com/in/ScottMalmberg" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+            <a href="https://github.com/ScottMalmberg" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","github"]}/></a>
+            <a href="https://linkedin.com/in/ScottMalmberg" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","linkedin"]}/></a>
         </div>
     </div>
     
