8261f1549b12cd33b986d2b7cbcb287bfab9dedc
Scott Malmberg <scott.malmberg@icloud.com>
2019-10-09 15:51:29 -0700

    Helmet, fixed topo background

--- src/styles/Layout.js
+++ src/styles/Layout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Nav from "../components/Nav"
 import MobileNav from '../components/MobileNav'
+import Helmet from 'react-helmet'
+import favicon from '../../public/favicon.ico'
 require("../styles/app.scss")
 
 
@@ -8,6 +10,17 @@ require("../styles/app.scss")
 export default ({children, image}) => 
     
     <div className="app-container">
+        <Helmet 
+            title="Scott Malmberg" 
+            meta={[
+                {
+                    name: "description",
+                    content: "Scott Malmberg - Front-End Developer"
+                }
+            ]}
+            link={[{rel:"shortcut icon", type: "image/x-icon", href: `${favicon}`},]}
+        />
+            
         <div className="image-container" style={{backgroundImage: image}}>
             <MobileNav />
         </div>
