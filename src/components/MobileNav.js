84073fa7bab4e1c3a21d3fa53bd789610bcefb50
Scott Malmberg <scott.malmberg@icloud.com>
2019-10-09 10:29:33 -0700

    Adjusting font-size and colors

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -5,9 +5,9 @@ export default () =>
         
         <div className="nav-container" id="mobile-nav">
             <nav> 
-                <strong><Link to="/" activeStyle={{color: '#F0F3BD'}}>Home</Link> |
-                <Link to="/about" activeStyle={{color: '#F0F3BD'}}> Words About Me</Link> | 
-                <Link to="/portfolio" activeStyle={{color: '#F0F3BD'}}> Things I've Made</Link>
+                <strong><Link to="/" activeStyle={{color: '#0093CA'}}>Home</Link> |
+                <Link to="/about" activeStyle={{color: '#0093CA'}}> Words About Me</Link> | 
+                <Link to="/portfolio" activeStyle={{color: '#0093CA'}}> Things I've Made</Link>
                 </strong>
             </nav>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,9 +5,9 @@ export default ({background}) =>
         
         <div className="nav-container" id="desktop-nav">
             <nav> 
-                <strong><Link to="/" activeStyle={{color: '#00A896'}}>Home</Link> |
-                <Link to="/about" activeStyle={{color: '#00A896'}}> Words About Me</Link> | 
-                <Link to="/portfolio" activeStyle={{color: '#00A896'}}> Things I've Made</Link></strong>
+                <strong><Link to="/" activeStyle={{color: '#0093CA'}}>Home</Link> |
+                <Link to="/about" activeStyle={{color: '#0093CA'}}> Words About Me</Link> | 
+                <Link to="/portfolio" activeStyle={{color: '#0093CA'}}> Things I've Made</Link></strong>
             </nav>
         </div>
  
