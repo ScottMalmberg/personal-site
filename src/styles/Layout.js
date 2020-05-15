3178c885ae91c88f8651fdbd89f7801c5ac8747a
Scott Malmberg <scottm@aloompa.com>
2020-05-15 09:48:09 -0500

    fix mobile styles

--- src/styles/Layout.js
+++ src/styles/Layout.js
@@ -6,11 +6,11 @@ require("../styles/app.scss")
 export default ({ children, image }) => (
   <div className="app-container">
     <div className="image-container" style={{ backgroundImage: image }}>
-      <MobileNav />
     </div>
     <div className="main-container">
       <Nav />
       {children}
     </div>
+      <MobileNav />
   </div>
 )
